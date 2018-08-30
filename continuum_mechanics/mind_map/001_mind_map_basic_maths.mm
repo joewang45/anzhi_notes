2a659047ab28a8cb5550bd6b7dca0e014fc9cf86
Anzhi <a.wang@stahlbau.tu-braunschweig.de>
2018-08-30 08:46:15 +0200

    work @ 29.08.2018

---
+++ continuum_mechanics/mind_map/001_mind_map_basic_maths.mm
@@ -0,0 +1,183 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node FOLDED="false" ID="ID_191153586" CREATED="1535459777791" MODIFIED="1535555046129" ICON_SIZE="36.0 pt" BACKGROUND_COLOR="#ffff00" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="0.0 cm" SHAPE_VERTICAL_MARGIN="0.0 cm" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER" MAX_WIDTH="7.0 cm" MIN_WIDTH="5.0 cm" BORDER_WIDTH="0.1 cm" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#333333"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      <img src="picture/icon_basic_math_new.png" height="300" width="300"/>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font SIZE="22"/>
+<hook NAME="MapStyle" zoom="0.751">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="sub_topic" COLOR="#000000" BACKGROUND_COLOR="#ff0000" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.0 mm" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#333333" VGAP_QUANTITY="1.0 pt">
+<font NAME="Bahnschrift" SIZE="18" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" COLOR="#ff0000" WIDTH="8"/>
+</stylenode>
+<stylenode TEXT="sub_sub_topic" COLOR="#ffffff" BACKGROUND_COLOR="#ff0000" STYLE="oval" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64.0 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32.0 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24.0 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24.0 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16.0 px">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="18" RULE="ON_BRANCH_CREATION"/>
+<edge COLOR="#ffffff"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node STYLE_REF="sub_topic" POSITION="right" ID="ID_273436263" CREATED="1535459777794" MODIFIED="1535555627971" VGAP_QUANTITY="1.0 pt" HGAP_QUANTITY="88.26829636690888 pt" VSHIFT_QUANTITY="-33.73170865732675 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="picture/icon_vector.png"/>
+      
+    </p>
+    <p style="text-align: center">
+      <font color="#ffffff">VECTOR</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Dot Product" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_588194298" CREATED="1535459777794" MODIFIED="1535554920715" HGAP_QUANTITY="63.00609623451081 pt" VSHIFT_QUANTITY="-53.54268226934772 pt" COLOR="#ffffff" BACKGROUND_COLOR="#ff0000" STYLE="oval">
+<edge STYLE="bezier" WIDTH="4"/>
+<node TEXT="\latex $\textcolor{red}{\mathbf{a} \cdot \mathbf{b} = a_i \mathbf{e}_i \cdot b_j \mathbf{e}_j = a_ib_j \delta_{ij}= a_ib_i}$" ID="ID_815316379" CREATED="1535553157619" MODIFIED="1535555624978" COLOR="#000000">
+<node TEXT="\latex $\textcolor{red}{&#xa;\mathbf{a}\cdot\mathbf{b}=0&#xa;\Leftrightarrow&#xa;\mathbf{a} \bot \mathbf{b} \\&#xa;}$" ID="ID_1752033704" CREATED="1535553374059" MODIFIED="1535555622822" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-13.49999959766866 pt"/>
+<node TEXT="\latex $\textcolor{red}{&#xa;|\mathbf{a}| = \sqrt{\mathbf{a} \cdot \mathbf{a}}&#xa;}$" ID="ID_1875530385" CREATED="1535553419689" MODIFIED="1535553445497"/>
+<node TEXT="\latex $\textcolor{red}{a_x = \mathbf{a} \cdot \mathbf{e}_x}$" ID="ID_1702917496" CREATED="1535555550878" MODIFIED="1535555624978" HGAP_QUANTITY="19.24999984353781 pt" VSHIFT_QUANTITY="17.249999485909953 pt"/>
+</node>
+</node>
+<node TEXT="Cross Product" STYLE_REF="sub_sub_topic" ID="ID_545950917" CREATED="1535554583355" MODIFIED="1535555627971" HGAP_QUANTITY="62.749998547136826 pt" VSHIFT_QUANTITY="-26.249999217689062 pt"/>
+<node TEXT="Coordiante Transformation" STYLE_REF="sub_sub_topic" ID="ID_1584502268" CREATED="1535554870490" MODIFIED="1535554917337" HGAP_QUANTITY="66.49999843537812 pt" VSHIFT_QUANTITY="55.49999834597116 pt"/>
+</node>
+<node STYLE_REF="sub_topic" POSITION="right" ID="ID_1910091850" CREATED="1535552817994" MODIFIED="1535555798761" BACKGROUND_COLOR="#0000ff" VGAP_QUANTITY="1.0 pt" HGAP_QUANTITY="52.99999883770947 pt" VSHIFT_QUANTITY="35.99999892711641 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="picture/icon_tensor.png"/>
+      
+    </p>
+    <p style="text-align: center">
+      <font color="#ffffff">TENSOR</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#0000ff"/>
+<node TEXT="Operation" STYLE_REF="sub_sub_topic" ID="ID_1695573282" CREATED="1535554740503" MODIFIED="1535555789995" BACKGROUND_COLOR="#0000ff" HGAP_QUANTITY="33.49999941885473 pt" VSHIFT_QUANTITY="49.499998524785084 pt">
+<node TEXT="simple contraction" ID="ID_103914419" CREATED="1535555800072" MODIFIED="1535555810484"/>
+</node>
+<node TEXT="Property" STYLE_REF="sub_sub_topic" ID="ID_942078715" CREATED="1535554740503" MODIFIED="1535555798761" BACKGROUND_COLOR="#0000ff" HGAP_QUANTITY="22.9999997317791 pt" VSHIFT_QUANTITY="6.749999798834331 pt"/>
+</node>
+<node STYLE_REF="sub_topic" POSITION="left" ID="ID_646995475" CREATED="1535552817994" MODIFIED="1535555735945" BACKGROUND_COLOR="#33cc00" HGAP_QUANTITY="52.99999883770947 pt" VSHIFT_QUANTITY="35.99999892711641 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="picture/icon_decomposition.png"/>
+      
+    </p>
+    <p style="text-align: center">
+      <font color="#ffffff">DECOMPOSITION</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<edge COLOR="#0000ff"/>
+<node TEXT="Single Contraction" STYLE_REF="sub_sub_topic" ID="ID_848064131" CREATED="1535554740503" MODIFIED="1535554764476" BACKGROUND_COLOR="#0000ff"/>
+</node>
+</node>
+</map>
